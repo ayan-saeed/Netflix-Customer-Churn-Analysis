3b4cc4a6ddf34721c3c76f503bffc76b647cc022
Ayan Saeed <danny4saeed@gmail.com>
2026-09-05 15:53:45 +0100

    number-of-profiles-churn-rate

--- SQL/netflix_dataset_analysis.sql
+++ SQL/netflix_dataset_analysis.sql
@@ -21,3 +21,22 @@ SELECT
     COUNT(*) as total_customers,
     ROUND((CAST(MAX(churned_customers) as DECIMAL) / CAST(COUNT(*) as DECIMAL)) * 100, 2) as churn_rate
 FROM netflix_customers, ChurnedCustomers;
+
+-- Churn concentration for number of profiles
+WITH ChurnedCustomersPerProfile as (
+    SELECT
+        number_of_profiles,
+        SUM(CASE
+            WHEN churned = 1 THEN 1 
+            ELSE 0 END) as number_of_churned,
+        COUNT(customer_id) as num_of_customers
+    FROM netflix_customers
+    GROUP BY number_of_profiles
+)
+
+SELECT
+    number_of_profiles,
+    ROUND(CAST(number_of_churned as DECIMAL) / CAST(num_of_customers as DECIMAL) * 100, 2) as churn_rate
+FROM ChurnedCustomersPerProfile
+ORDER BY number_of_churned;
+
